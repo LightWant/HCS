ce8d47e1d81fde5084aece30e125608614154cd1
LightWant <2510035537@qq.com>
2022-11-07 15:02:37 +0800

    first commit

---
+++ src/plex/CMakeFiles/d2k.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/d2k.dir/d2ksginC.cpp.o"
+  "CMakeFiles/d2k.dir/d2ksginC.cpp.o.d"
+  "libd2k.a"
+  "libd2k.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/d2k.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/tools/types.hpp
@@ -0,0 +1,7 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <cstdint>
+using ui = uint32_t;
+
+#endif
